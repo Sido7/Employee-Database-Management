2d8802836e3f3d5bb1097ac56067c635fce0c08a
siddharth <siddharth1850@gmail.com>
2022-08-09 14:48:47 +0530

    commit 2

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ var app = express()
 var ejs = require('ejs')
 var fs = require('fs')
 var path = require('path')
+var dotenv = require('dotenv')
 
+dotenv.config({path: path.join(__dirname,'config.env')})
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 app.use(express.static(__dirname + '/public'))
+
+var port  = process.env.PORT || 4000
+app.listen(port,(err)=>{
+    console.log("listening to port"+port)
+})
